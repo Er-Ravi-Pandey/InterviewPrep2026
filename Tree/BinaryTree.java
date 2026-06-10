409d96dbb03364711ddfdc9a7075e32d1fe3847a
ravi29pandey <erravipandeybbd@gmail.com>
2026-06-10 17:49:56 +0530

    Day21

---
+++ Tree/BinaryTree.java
@@ -0,0 +1,62 @@
+package com.DSA.Tree;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+
+    public void inorder(Node root) {
+        if (root == null)
+            return;
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public void preorder(Node root) {
+        if (root == null)
+            return;
+
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public void postorder(Node root) {
+        if (root == null)
+            return;
+
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        BinaryTree tree = new BinaryTree();
+
+        System.out.println("Inorder traversal:");
+        tree.inorder(root);
+
+        System.out.println("\nPreorder traversal:");
+        tree.preorder(root);
+
+        System.out.println("\nPostorder traversal:");
+        tree.postorder(root);
+    }
+}
